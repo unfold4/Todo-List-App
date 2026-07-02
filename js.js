d0723a2d029a3bbd14337b5617f69707793e2385
unfold4 <unfold4s-actual-email@example.com>
2026-07-02 22:25:41 +0530

    feat: add task filtering, delete functionality and UI improvements

--- js.js
+++ js.js
@@ -35,8 +35,12 @@ function addTask(){
   let btn = document.querySelector(".third .content button")
   let taskVal
 
+    
+
   input.addEventListener("keydown", function(e){
+
     if(e.key==="Enter" && input.value!==""){
+  if(document.querySelector(".lastSection .main p").textContent!="") document.querySelector(".lastSection .main p").textContent="";
     tasks.all.push(input.value)
     tasks.pending.push(input.value)
     taskVal=input.value
@@ -46,6 +50,7 @@ function addTask(){
   })
   
   btn.addEventListener("click",function(){
+  if(document.querySelector(".lastSection .main p").textContent!="") document.querySelector(".lastSection .main p").textContent="";
     if(input.value!==""){
     tasks.all.push(input.value)
     tasks.pending.push(input.value)
@@ -67,10 +72,18 @@ function displayCards3Btns(){
   let selectAll = new Event("click")
 
   all.addEventListener("click",function(){
-    if(activeFilter===all) return
+    // if(activeFilter===all) return
+
+
+    document.querySelectorAll(".cards").forEach(function(card){
+      card.remove()
+    })
+
     activeFilter=all;
-    all.style.color="#C7F441";
-    all.style.borderColor="#C7F441"
+    all.classList.add("active")
+    done.classList.remove("active")
+    pending.classList.remove("active")
+
 
     if(tasks.all.length===0) document.querySelector(".lastSection .main p").textContent="No tasks here. Add one above.";
     else {document.querySelector(".lastSection .main p").textContent="";
@@ -81,12 +94,20 @@ function displayCards3Btns(){
   })
 
   pending.addEventListener("click",function(){
-    if(activeFilter===pending) return
-    activeFilter=pending;
-    pending.style.color="#C7F441";
-    pending.style.borderColor="#C7F441"
+    // if(activeFilter===pending) return
 
-    if(tasks.all.length===0) document.querySelector(".lastSection .main p").textContent="No tasks here. Add one above.";
+      document.querySelectorAll(".cards").forEach(function(card){
+      card.remove()
+    })
+
+    activeFilter=pending;
+
+    done.classList.remove("active")
+    all.classList.remove("active")
+    pending.classList.add("active")
+
+    if(tasks.pending.length===0) document.querySelector(".lastSection .main p").textContent="No tasks here. Add one above.";
+
     else {document.querySelector(".lastSection .main p").textContent="";
       tasks.pending.forEach(function(x){
       addTaskCard(x)
@@ -95,24 +116,93 @@ function displayCards3Btns(){
   })
 
     done.addEventListener("click",function(){
-    if(activeFilter===done) return
-    activeFilter=done;
-    done.style.color="#C7F441";
-    done.style.borderColor="#C7F441"
+    // if(activeFilter===done) return
 
-    if(tasks.all.length===0) document.querySelector(".lastSection .main p").textContent="No tasks here. Add one above.";
+      document.querySelectorAll(".cards").forEach(function(card){
+      card.remove()
+    })
+
+    activeFilter=done;
+    done.classList.add("active")
+    all.classList.remove("active")
+    pending.classList.remove("active")
+
+
+    if(tasks.completed.length===0) document.querySelector(".lastSection .main p").textContent="No tasks here. Add one above.";
     else {document.querySelector(".lastSection .main p").textContent="";
-      tasks.done.forEach(function(x){
+      tasks.completed.forEach(function(x){
       addTaskCard(x)
     })}
 
+
   })
 
+
+function clickCross(){
+
+  let click = new Event("click")
+
+  document.querySelector(".lastSection .main").addEventListener("click",function(e){
+
+    if(e.target.tagName==="BUTTON") {
+      let cardToRemove=e.target.closest(".cards")
+      let taskTxt=cardToRemove.querySelector(".check label").textContent
+      removeTaskFromArr(taskTxt)
+      activeFilter.dispatchEvent(click)
+      }
+
+    if(e.target.tagName=="INPUT"){
+      let el = e.target;
+      let taskk=el.closest(".check").querySelector("label").innerText
+  
+      let state=el.checked
+      if(state){
+        for(let i=0;i<tasks.pending.length;i++){
+          if(tasks.pending[i]===taskk) tasks.pending.splice(i,1)
+        }
+      tasks.completed.push(taskk);
+      }
+      else{
+        for(let i=0;i<tasks.completed.length;i++){
+          if(tasks.pending[i]===taskk) tasks.pending.splice(i,1)
+        }
+      tasks.pending.push(taskk);
+      }
+
+    }
+
+  })
+}
+
   all.dispatchEvent(selectAll);
+  clickCross()
+  addTask()
+}
+
+function checkUncheck(){
+  let inputBox = document.querySelector(".lastSection .main .cards .check input")
+  inputBox.addEventListener("click",function(){
+    console.log(inputBox.check)
+  })
+
+}
+
+
+
+function removeTaskFromArr(x){
+  for(let i=0;i<tasks.all.length;i++){
+    if(tasks.all[i]===x) tasks.all.splice(i,1)
+  }
+  for(let i=0;i<tasks.pending.length;i++){
+    if(tasks.pending[i]===x) tasks.pending.splice(i,1)
+  }
+  for(let i=0;i<tasks.completed.length;i++){
+    if(tasks.completed[i]===x) tasks.completed.splice(i,1)
+  }
 
 }
 
 
 displayTime()
 displayCards3Btns()
-addTask()
+// addTask()
